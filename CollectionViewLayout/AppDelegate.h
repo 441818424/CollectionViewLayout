0549674eff2fe6116b004f3fda7253386b493ce2
xiangjune <PAD_Chenxiang_MAC@pad-chenxiang-mac.local>
2018-01-18 15:41:22 +0800

    Initial Commit

---
+++ CollectionViewLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewLayout
+//
+//  Created by PAD_Chenxiang_MAC on 2018/1/18.
+//  Copyright © 2018年 Tianwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewLayout
+//
+//  Created by PAD_Chenxiang_MAC on 2018/1/18.
+//  Copyright © 2018年 Tianwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
